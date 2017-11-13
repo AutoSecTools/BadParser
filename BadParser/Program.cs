f63ecf81d4743c5e3e550e75085916c9450cd627
John Leitch <JohnLeitch@outlook.com>
2017-11-12 21:05:48 -0800

    Removed C# classes to Aphid, added XML support.

--- BadParser/Program.cs
+++ BadParser/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Script.Serialization;
